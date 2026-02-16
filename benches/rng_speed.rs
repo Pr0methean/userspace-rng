dea79a70e31abd0339874ae4083bcf4ee5f56690
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-15 16:40:45 -0800

    Fix build error in benchmark (outdated import)

--- benches/rng_speed.rs
+++ benches/rng_speed.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use userspace_random::random256;
+use userspace_rng::random256;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("random256", |b| b.iter(|| random256()));
